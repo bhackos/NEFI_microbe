ccf640e75e4932764fc84e16770209476f909a66
Briana Hackos <BrianaHackos@dhcp-wifi-8021x-155-41-76-17.bu.edu>
2019-06-28 10:05:57 -0400

    cleaned up code to start making a readme file

--- BRITE_2019/metagenomeCode.R
+++ BRITE_2019/metagenomeCode.R
@@ -1,12 +1,3 @@
-# #download nitrogen data 
-# NTR <- loadByProduct(dpID="DP1.10080.001", site="all", startdate="2017-03", enddate="2019-03",
-#                      package="expanded", check.size=F)
-# 
-# #nitrogen rates calculation package
-# out <- def.calc.ntrans(kclInt = NTR$ntr_internalLab, 
-#                        kclIntBlank = NTR$ntr_internalLabBlanks, 
-#                        kclExt = NTR$ntr_externalLab, 
-#                        soilMoist = NTR$sls_soilMoisture)
 source("BRITE-paths.r")
 library(neonUtilities)  
 library(neonNTrans) 
@@ -15,40 +6,13 @@ library(ggplot2)
 out <- readRDS(N_data_path)
 meta.dat <- loadByProduct(site = "all", dpID = "DP1.10107.001", package="basic", check.size=F)
 seq <- meta.dat[["mms_metagenomeSequencing"]]
-#dim = 80 rows
-
-# pseudocode
 
 # subset both dfs (out and seq) to 2017
-# something like: substr(out$dateID, 1, 4) == "2017"
 n_2017 <- out[substr(out$dateID, 1, 4) == "2017",]
 seq_2017 <- seq[substr(seq$collectDate, 1, 4) == "2017", ]
 
 # create new mergeID for both
 # first part of sampleID (without date)
-# i <- 1
-# while (i < 1709) {
-#   initial <- n_2017$sampleID[i]
-#   final <- substr(n_2017$sampleID[i], 1, nchar(as.character(n_2017$sampleID[i]))-9)
-#   n_2017$sampleID <- sub(initial, final, n_2017$sampleID)
-#   i = i + 1
-# }
-
-# n_2017$mergeSampleID <- NA
-# for (i in unique(n_2017$incubationPairID)){
-#   samp <- unique(n_2017$incubationPairID)[i]
-#   samp_rows <- n_2017[n_2017$incubationPairID == samp,]
-#   samp_first_date <- samp_rows[samp_rows$nTransBoutType=="tInitial",]$dateID
-#   mergeSampleID <- paste0(substr(samp_rows$sampleID[i], 1, 10), "-", samp_first_date)
-#   n_2017[n_2017$incubationPairID == samp,]$mergeSampleID <- mergeSampleID
-# }
-
-# in N_data, create ID with site/plot/year-month
-# new <- n_2017
-# but <- new[complete.cases(new[ , 1]), ]
-# but2 <- but[complete.cases(but[ , 98]), ]
-# but3 <- but2[complete.cases(but2[, 63]), ]
-
 i <- 1622
 n_2017 <- n_2017[!duplicated(n_2017$uid.y),]
 
@@ -72,44 +36,21 @@ for (i in 1:209) {
   seq_2017[seq_2017$dnaSampleID == samp,]$mergeSampleID <- mergeSampleID
 }
 
-
-# i <- 1
-# while (i < 1708) {
-#   initial <- n_2017$sampleID[i]
-#   final <- substr(n_2017$sampleID[i], 1, 10)
-#   n_2017$sampleID <- sub(initial, final, n_2017$sampleID)
-#   i = i + 1
-# }
-# 
-# j <- 1
-# while (j < 210) {
-#   binitial <- seq_2017$dnaSampleID[j]
-#   bfinal <- substr(seq_2017$dnaSampleID[j], 1, 10)
-#   seq_2017$dnaSampleID <- sub(binitial, bfinal, seq_2017$dnaSampleID)
-#   j = j + 1
-# }
-
 # subset nitrogen data to nTransBoutType.x == "tFinal"
 n_2017 <- n_2017[n_2017$nTransBoutType == "tFinal", ]
 
 # merge by mergeID
-
 merge1 <- merge(n_2017, seq_2017, by = "mergeSampleID")
-# print dim, print the # of plots at each site
-
-# make a boxplot of the N data - 2 plots (one for Nmin, one for Nitrification)
-# separated by siteID, like https://plot.ly/r/box-plots/
-# or use ggplot2, or base R
 
 #get rid of duplicated columns 
 newmerge <- merge1[!duplicated(as.list(merge1))]
 
-#N min
+#N min boxplot separated by siteID
 nmin_plot <- ggplot(newmerge, aes(x= siteID.x, y= netNminugPerGramPerDay)) +
   geom_boxplot()
 nmin_plot
 
-#nitrification
+#nitrification boxplot separated by siteID
 nit_plot <- ggplot(newmerge, aes(x= siteID.x, y= netNitugPerGramPerDay)) +
   geom_boxplot()
 nit_plot
