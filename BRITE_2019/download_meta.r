c59d3c089fc9150f726888b3ab22065c8d999a59
Briana Hackos <brha6910@colorado.edu>
2019-06-28 14:31:24 -0400

    edited the code so that tic and toc work. also made it more readable and added comments

--- BRITE_2019/download_meta.r
+++ BRITE_2019/download_meta.r
@@ -1,26 +1,25 @@
-#### download code - make separate file ####
+#### download code ####
 library(RCurl)
 library(utils)
 # source functions:
+setwd("BRITE_2019/") # assumes you're in NEFI_microbe
+source("BRITE-paths.r")
 source("../NEFI_functions/tic_toc.r")
 
 # read in the file that Lee sent, with the urls
-meta.data.file <- read.delim("Microbial_metagenome_sequences,_Level_0_mms_rawDataFiles_in 4.txt")
+meta.data.file <- read.delim("Microbial_metagenome_sequences.txt")
 
 # set a path for where things should download
 download.dir <- "/projectnb/talbot-lab-data/NEFI_data/metagenomes/raw_sequences/" 
 
-#download.file(url=url, destfile = paste0(download.dir, basename(url)))
-i <- 1
-# while (i < 35) {
-  
-  m.url <- meta.data.file[i, 15]
-  download.file(url=as.character(m.url), destfile = paste0(download.dir, basename(as.character(m.url))))
-  # i = i+1
-# }
-
 # loop through list of URLs
 # time the downloads
-# tic()
-# download
-# toc()
+i <- 1
+while (i < 35) {
+  
+  m.url <- meta.data.file[i, 15]
+  tic()
+  download.file(url=as.character(m.url), destfile = paste0(download.dir, basename(as.character(m.url))))
+  toc()
+  i = i+1
+}
