2c292753b7b43a990fdca6f152b869edd27db2d3
Briana Hackos <brha6910@colorado.edu>
2019-07-11 10:55:09 -0400

    added specifications

--- BRITE_2019/qsubfile.sh
+++ BRITE_2019/qsubfile.sh
@@ -1,4 +1,13 @@
+
 #!/bin/bash -l
+
+#$ -l h_rt=120:00:00
+
+#$ -l mem_total=125G -pe omp 16
+
+#$ -N DNA_decontam
+
+#$ -m ae
 
 sunbeam run -- --configfile /projectnb/talbot-lab-data/NEFI_data/my_project/sunbeam_config.yml all_decontam
 
