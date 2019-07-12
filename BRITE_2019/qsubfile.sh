617ee7c4ca76cb2d5a652994ddaea0f5d6cf56a7
Briana Hackos <brha6910@colorado.edu>
2019-07-12 09:26:57 -0400

    deleted extra hyphens

--- BRITE_2019/qsubfile.sh
+++ BRITE_2019/qsubfile.sh
@@ -9,6 +9,6 @@
 
 #$ -m ae
 
-sunbeam run -- --configfile /projectnb/talbot-lab-data/NEFI_data/my_project/sunbeam_config.yml all_decontam
+sunbeam run --configfile /projectnb/talbot-lab-data/NEFI_data/my_project/sunbeam_config.yml all_decontam
 
 
