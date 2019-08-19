7f58d929c16fe614fccaee2e28eb82a9d0272156
bhackos <brha6910@colorado.edu>
2019-08-19 14:22:47 -0400

    qsub for sunbeam quality control step

---
+++ BRITE_2019/5.qsub_quality_control.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash -l
+
+#$ -l h_rt=120:00:00
+
+#$ -l mem_total=9G -pe omp 28
+
+#$ -N DNA_QC
+
+#$ -m ae
+
+cd /usr4/spclpgm/bhackos
+
+module load miniconda/4.5.12
+
+source activate sunbeam-tmp.pkm3BCVHjt
+
+unset PYTHONPATH
+
+sunbeam run -- --configfile /projectnb/talbot-lab-data/NEFI_data/metagenomes/2017_project/sunbeam_config.yml --unlock all_qc
+
+sunbeam run -- --configfile /projectnb/talbot-lab-data/NEFI_data/metagenomes/2017_project/sunbeam_config.yml all_qc
+
+
